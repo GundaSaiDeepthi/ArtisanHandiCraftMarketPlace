b7d82b3561e2f6474ac9edc09ef53b06cc621132
GundaSaiDeepthi <23eg105r52@anurag.edu.in>
2026-06-17 00:29:52 +0530

    Updateddd

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,5 +1,6 @@
 import nodemailer from "nodemailer";
-
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 /*
 ==================================================
 CREATE EMAIL TRANSPORTER
